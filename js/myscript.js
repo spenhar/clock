3c472d7095256e29ecb03239b6d421e1510c57f4
Spencer Harrison <spencerjharrison@gmail.com>
2014-10-09 13:55:42 -0400

    first commit

---
+++ js/myscript.js
@@ -0,0 +1,18 @@
+function startTime() {
+    var today=new Date();
+    var h=today.getHours();
+    var m=today.getMinutes();
+    var s=today.getSeconds();
+    h = checkTime(h);
+    m = checkTime(m);
+    s = checkTime(s);
+    // document.getElementById('txt').innerHTML = h+":"+m+":"+s;
+    document.getElementById('hours').innerHTML = h;
+    document.getElementById('minutes').innerHTML = m;
+    var t = setTimeout(function(){startTime()},500);
+}
+
+function checkTime(i) {
+    if (i<10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
